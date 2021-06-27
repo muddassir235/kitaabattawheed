d98af992a2898c82c667d58a098ce8db9ad6e003
Muddassir Khan <muddassir.ahmed235@gmail.com>
2021-06-27 14:58:22 +0400

    Refactor macros

--- app/src/main/java/com/app/kitaabattawheed/macros.kt
+++ app/src/main/java/com/app/kitaabattawheed/macros.kt
@@ -3,14 +3,11 @@ package com.app.kitaabattawheed
 import android.content.Context
 import android.content.res.Resources.Theme
 import android.util.TypedValue
-import androidx.annotation.ColorInt
-
 
 fun Context.attrColor(colorId: Int): Int {
     val typedValue = TypedValue()
-    val theme: Theme = this.getTheme()
+    val theme: Theme = this.theme
     theme.resolveAttribute(colorId, typedValue, true)
-    @ColorInt val color = typedValue.data
 
-    return color
+    return typedValue.data
 }
