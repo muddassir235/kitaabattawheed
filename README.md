8416ed56521ffca9ca7ea19a4f009726c6bb2dca
Bu Muhammad <bumuhammad@Bus-MacBook-Air.local>
2025-06-13 00:21:50 +0400

    Add more translations. Fix some issues.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Kitaab At Tawheed كتاب التوحيد
 
-Kitaab at Tawheed. The right of Allah on his servents.
+Kitaab at Tawheed. The right of Allah on his servants.
 
 Play Store Listing: https://play.google.com/store/apps/details?id=com.app.kitaabattawheed
--- app/src/androidTest/java/com/app/kitaabattawheed/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/app/kitaabattawheed/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.kitaab_at_tawheed", appContext.packageName)
+        assertEquals("com.app.kitaabattawheed", appContext.packageName)
     }
 }
